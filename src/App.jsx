d6f633e2708b34b448f34c7ec4e74d305b1baee9
christian_reynaltt <creynaltt@maristak.net>
2023-08-24 13:25:41 +0200

    feature:  todos los apartados añadidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from '@/components/Home.jsx'
-import { Main } from './components/Main'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import Home from "@/components/Home.jsx"
+import Main from "@/components/Main"
+import Skills from "@/components/Skills"
+import Footer from "@/components/Footer"
 
 function App() {
-    return (
-        <>
-            <Home />
-            <Main />
-        </>
-    )
+  return (
+    <>
+      <Home />
+      <Main />
+      <Skills />
+      <Footer />
+    </>
+  )
 }
 
 export default App
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react"
+import styles from "./Main.module.css"
 
-export const Main = () => {
-    return (
-        <div><div>I'm Christian Reynaltt </div></div>
-    )
+const Main = () => {
+  return (
+    <div className={styles.main}>
+      <div>I'm Christian Reynaltt</div>
+      <div>
+        A Frontend focused Web Developer building the Frontend of Websites and Web
+        Applications and interested in Big data and new Web technologies
+      </div>
+    </div>
+  )
 }
+
+export default Main
