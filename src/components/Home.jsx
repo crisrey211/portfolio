7b84967d6d3b7829e895832aceb546bb2bf805e7
Christian Reynaltt <90900049+crisrey211@users.noreply.github.com>
2023-08-24 10:50:25 +0200

    Firs commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './Home.module.css'
+
+import viteLogo from '/vite.svg'
+import parqueNAtural from '@/assets/parqueNAtural.jpg'
+import reactLogo from '@/assets/react.svg'
+
+const Home = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Projects</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Home
