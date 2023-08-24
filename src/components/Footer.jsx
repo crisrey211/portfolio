54bf9d728e3eb3d172b7a80720d3e8db75782f3d
christian_reynaltt <creynaltt@maristak.net>
2023-08-24 13:56:06 +0200

    not finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div>
+      <div>
+        Contact me
+        <a href="https://www.linkedin.com/in/christianreynaltt">LinkedIn</a>
+      </div>
+      <div>
+        <a href="https://github.com/crisrey211">Github</a>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,6 +9,12 @@ const Main = () => {
         A Frontend focused Web Developer building the Frontend of Websites and Web
         Applications and interested in Big data and new Web technologies
       </div>
+      <div>
+        I am a responsible, serious person who is eager to work and learn. As an organised
+        and highly motivated person, I am able to adapt myself to any circumstance and
+        always give the best of myself in any project, at the same time. I strive to work
+        in a team and to develop myself personally and professionally.
+      </div>
     </div>
   )
 }
