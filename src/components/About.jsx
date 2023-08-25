751706e8a2cb42acedbd4cdd6270aae8f3ae563a
christian_reynaltt <creynaltt@maristak.net>
2023-08-25 21:02:20 +0200

    not finished About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,23 +5,27 @@ const About = () => {
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        <div className={styles.name}>BRO, I'M CHRISTIAN REYNALTT</div>
+        <text className={styles.name}>BRO, I'M CHRISTIAN REYNALTT</text>
         <span className={styles.paragraph1}>
           A Frontend focused Web Developer building the Frontend of Websites and Web
           Applications and interested in Big data and new Web technologies
         </span>
       </div>
       <div className={styles.about} id="about">
-        <h5>About me</h5>
-        <div className={styles.paragraph2}>
-          I am a responsible, serious person who is eager to work and learn. As an
-          organised and highly motivated person, I am able to adapt myself to any
-          circumstance and always give the best of myself in any project, at the same
-          time. I strive to work in a team and to develop myself personally and
-          professionally.
-        </div>
-        <h5>Work experience</h5>
-        <div className={styles}>CMZ</div>
+        <section className={styles.me}>
+          <h5 className={styles.titulo}>ABOUT ME</h5>
+          <p className={styles.section}>
+            I am a responsible, serious person who is eager to work and learn. As an
+            organised and highly motivated person, I am able to adapt myself to any
+            circumstance and always give the best of myself in any project, at the same
+            time. I strive to work in a team and to develop myself personally and
+            professionally.
+          </p>
+        </section>
+        <section className={styles.work}>
+          <h5 className={styles.titulo}>WORK EXPERIENCE</h5>
+          <span>CMZ</span>
+        </section>
       </div>
     </div>
   )
