6a7889fe046b1442cbfa2132b3bb197ba9acdb5a
christian_reynaltt <creynaltt@maristak.net>
2023-08-26 18:24:35 +0200

    new logos svg added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        <text className={styles.name}>BRO, I'M CHRISTIAN REYNALTT</text>
+        <h1 className={styles.name}>I'm Christian Reynaltt</h1>
         <span className={styles.paragraph1}>
           A Frontend focused Web Developer building the Frontend of Websites and Web
           Applications and interested in Big data and new Web technologies
@@ -13,17 +13,14 @@ const About = () => {
       </div>
       <div className={styles.about} id="about">
         <section className={styles.me}>
-          <h5 className={styles.titulo}>ABOUT ME</h5>
+          <h5 className={styles.titulo}>About me</h5>
           <p className={styles.section}>
-            I am a responsible, serious person who is eager to work and learn. As an
-            organised and highly motivated person, I am able to adapt myself to any
-            circumstance and always give the best of myself in any project, at the same
-            time. I strive to work in a team and to develop myself personally and
-            professionally.
+            Free developers from time-consuming, unnecessary processes that slow your
+            work, so you and your team can focus on creating.
           </p>
         </section>
         <section className={styles.work}>
-          <h5 className={styles.titulo}>WORK EXPERIENCE</h5>
+          <h5 className={styles.titulo}>Work experience</h5>
           <span>CMZ</span>
         </section>
       </div>
