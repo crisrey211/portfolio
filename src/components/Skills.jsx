600770362cb1f78a0d1d0e933709c9aa0fcce977
christian_reynaltt <creynaltt@maristak.net>
2023-08-28 19:33:38 +0200

    FIX: cambio estrcutura

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import ccs3 from "./../assets/css3Logo.svg"
 const Skills = () => {
   return (
     <div className={styles.main} id="skills">
-      <h5>Skills</h5>
+      <h5>Professional experience</h5>
       <div className={styles.logos}>
         <div className={styles.logo}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
