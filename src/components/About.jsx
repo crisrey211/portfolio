5da5cc17f4c105bc4f2083521b7a47b8cbc3fa68
christian_reynaltt <creynaltt@maristak.net>
2023-09-14 20:28:00 +0200

    ABOUT: a falta de detalles Desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className={styles.about} id="about">
       <section className={styles.work}>
-        <div className={styles.christian}>
+        <div className={styles.leftSide}>
           <h5 className={styles.titulo}>Mi foto</h5>
           <div className={styles.fotoContainer}>
             <img src="../../src/assets/chris.jpg" />
@@ -13,16 +13,21 @@ const About = () => {
         </div>
       </section>
       <section className={styles.me}>
-        <h5 className={styles.titulo}>About me</h5>
-        <p className={styles.section}>
-          Soy un desarrollador Front-end Web que crea diversas aplicaciones entendiendo el
-          producto y buscando su éxito. Mira algunos de mis proyectos personales.
-        </p>
-        <p className={styles.section}>
-          Me gusta compartir los conocimientos que he adquirido a lo largo de mi carrera
-          como desarrollador, por lo que puedo ayudar a la gente en lo que pueda. Siente
-          libre de contactarme o seguirme en LinkedIn.
-        </p>
+        <div className={styles.rightSide}>
+          <h5 className={styles.titulo}>About me</h5>
+          <div className={styles.information}>
+            <p className={styles.section}>
+              Soy un desarrollador Front-end Web que crea diversas aplicaciones
+              entendiendo el producto y buscando su éxito. Mira algunos de mis proyectos
+              personales.
+            </p>
+            <p className={styles.section}>
+              Me gusta compartir los conocimientos que he adquirido a lo largo de mi
+              carrera como desarrollador, por lo que puedo ayudar a la gente en lo que
+              pueda. Siente libre de contactarme o seguirme en LinkedIn.
+            </p>
+          </div>
+        </div>
       </section>
     </div>
   )
