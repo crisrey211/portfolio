f00ef8292e826201e4a37f54e372d72fa0e27aa5
christian_reynaltt <creynaltt@maristak.net>
2023-09-16 00:26:32 +0200

    NAVBAR: refactor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,28 +3,21 @@ import MenuIcon from "./icons/MenuIcon"
 import styles from "./NavBar.module.css"
 
 const Home = () => {
+  const menuItems = ["Home", "About", "Stack", "Projects", "Contact"]
+
   return (
     <nav className={styles.containerHome}>
       <div className={styles.name}>Christian</div>
       <ul className={styles.navbar}>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#stack">Stack</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
+        {menuItems.map((item) => (
+          <li>
+            <a href={`#${item.toLocaleLowerCase()}`}>{item}</a>
+          </li>
+        ))}
       </ul>
       <div className={styles.iconNav}>
         <MenuIcon />
+        <div></div>
       </div>
     </nav>
   )
