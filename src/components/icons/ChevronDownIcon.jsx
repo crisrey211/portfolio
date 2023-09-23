feca22e1ffa55ff37c1060a573bb14d79761d1ce
christian_reynaltt <creynaltt@maristak.net>
2023-09-23 02:29:11 +0200

    hotfix: accordion boton arregladi

--- src/components/icons/ChevronDownIcon.jsx
+++ src/components/icons/ChevronDownIcon.jsx
@@ -4,11 +4,11 @@ const ChevronUpIcon = ({ className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="24"
+      height="24"
       fill="currentColor"
       className={className}
-      viewBox="0 0 16 16"
+      viewBox="0 0 24 24"
     >
       <path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />{" "}
     </svg>
--- src/components/icons/ChevronUpIcon.jsx
+++ src/components/icons/ChevronUpIcon.jsx
@@ -4,8 +4,8 @@ const ChevronUpIcon = ({ className }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="24"
+      height="24"
       fill="currentColor"
       className={className}
       viewBox="0 0 16 16"
